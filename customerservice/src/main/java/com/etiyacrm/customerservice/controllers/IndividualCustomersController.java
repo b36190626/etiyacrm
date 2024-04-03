f0d9178cccd9566c1b6c6870f8106334eda3cac5
B.Kaan Yumak <56797933+b36190626@users.noreply.github.com>
2024-04-03 14:48:26 +0300

    Add files via upload
    add Controller

---
+++ customerservice/src/main/java/com/etiyacrm/customerservice/controllers/IndividualCustomersController.java
@@ -0,0 +1,45 @@
+package com.etiyacrm.customerservice.controllers;
+
+import com.etiyacrm.customerservice.core.business.paging.PageInfo;
+import com.etiyacrm.customerservice.services.abstracts.IndividualCustomerService;
+import com.etiyacrm.customerservice.services.dtos.requests.IndividualCustomerRequests.CreateIndividualCustomerRequest;
+import com.etiyacrm.customerservice.services.dtos.requests.IndividualCustomerRequests.UpdateIndividualCustomerRequest;
+import com.etiyacrm.customerservice.services.dtos.responses.CityResponses.GetAllCityResponse;
+import com.etiyacrm.customerservice.services.dtos.responses.IndividualCustomerResponses.CreatedIndividualCustomerResponse;
+import com.etiyacrm.customerservice.services.dtos.responses.IndividualCustomerResponses.GetAllIndividualCustomerResponse;
+import com.etiyacrm.customerservice.services.dtos.responses.IndividualCustomerResponses.UpdatedIndividualCustomerResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/individualcustomers")
+public class IndividualCustomersController {
+    private IndividualCustomerService individualCustomerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add")
+    public CreatedIndividualCustomerResponse add(@Valid @RequestBody CreateIndividualCustomerRequest createIndividualCustomerRequest){
+        return individualCustomerService.add(createIndividualCustomerRequest);
+    }
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update")
+    public UpdatedIndividualCustomerResponse update(@Valid @RequestBody UpdateIndividualCustomerRequest updateIndividualCustomerRequest,
+                                                    @PathVariable Long id){
+        return individualCustomerService.update(updateIndividualCustomerRequest, id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "GetAll")
+    public List<GetAllIndividualCustomerResponse> getAll(PageInfo pageInfo){
+        return individualCustomerService.getAll(pageInfo);
+    }
+}

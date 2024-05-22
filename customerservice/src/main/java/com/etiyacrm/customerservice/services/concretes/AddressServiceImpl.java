eda70ef49970ee2f9dbe302a198c0c1172a65c90
mustafa.tatlikatik <mustafa.tatlikatik@etiya.com>
2024-05-22 13:55:55 +0300

    adress-fix

--- customerservice/src/main/java/com/etiyacrm/customerservice/services/concretes/AddressServiceImpl.java
+++ customerservice/src/main/java/com/etiyacrm/customerservice/services/concretes/AddressServiceImpl.java
@@ -98,8 +98,9 @@ public class AddressServiceImpl implements AddressService {
     public DeletedAddressResponse delete(String id) {
         Address address = addressRepository.findById(id).get();
         addressBusinessRules.checkIfAddressDeleted(address.getDeletedDate());
-        addressBusinessRules.cantDeleteLastAddress(id);
+        boolean isLastAddress = addressBusinessRules.cantDeleteLastAddress(id);
         address.setId(id);
+        address.setDefaultAddress(isLastAddress);
         address.setDeletedDate(LocalDateTime.now());
         Address deletedAddress = addressRepository.save(address);
 
